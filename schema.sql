ef5cf37fc05e65f152f65135700ed5f694548875
unzipnav <unzip.nav@gmail.com>
2023-04-25 06:37:59 +0530

    updated schema.sql file

--- schema.sql
+++ schema.sql
@@ -41,6 +41,15 @@ CREATE TABLE `comment` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `comment`
+--
+
+LOCK TABLES `comment` WRITE;
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `comment_vote`
 --
 
@@ -51,13 +60,22 @@ CREATE TABLE `comment_vote` (
   `user_id` bigint NOT NULL,
   `comment_id` bigint NOT NULL,
   `created_on` datetime DEFAULT NULL,
-  `vote_type` enum('UP_VOTE','DOWN_VOTE') DEFAULT NULL,
-  PRIMARY KEY (`user_id`,`comment_id`),
+  `vote_type` enum('UP_VOTE','DOWN_VOTE') NOT NULL,
+  PRIMARY KEY (`user_id`,`comment_id`,`vote_type`),
   KEY `comment_id` (`comment_id`),
-  CONSTRAINT `comment_vote_ibfk_1` FOREIGN KEY (`comment_id`) REFERENCES `comment` (`id`),
-  CONSTRAINT `comment_vote_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+  CONSTRAINT `comment_vote_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `comment_vote_ibfk_2` FOREIGN KEY (`comment_id`) REFERENCES `comment` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comment_vote`
+--
+
+LOCK TABLES `comment_vote` WRITE;
+/*!40000 ALTER TABLE `comment_vote` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comment_vote` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `post`
@@ -80,6 +98,16 @@ CREATE TABLE `post` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `post`
+--
+
+LOCK TABLES `post` WRITE;
+/*!40000 ALTER TABLE `post` DISABLE KEYS */;
+INSERT INTO `post` VALUES (1,1,'ez vs navi ggwp','2023-04-25 12:00:00',0,0),(2,1,'well played zywoo, you are next superstar','2023-04-23 12:00:00',0,0),(3,3,'see you all at the premire of my show','2023-04-24 12:00:00',0,0),(4,3,'great movie charlie. Keep it up','2023-04-25 10:00:00',0,0),(5,2,'power is power','2023-04-24 10:00:00',0,0),(6,4,'knowledge is power','2023-04-24 09:55:00',0,0);
+/*!40000 ALTER TABLE `post` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `post_vote`
 --
 
@@ -99,6 +127,15 @@ CREATE TABLE `post_vote` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `post_vote`
+--
+
+LOCK TABLES `post_vote` WRITE;
+/*!40000 ALTER TABLE `post_vote` DISABLE KEYS */;
+/*!40000 ALTER TABLE `post_vote` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `user`
 --
 
@@ -113,6 +150,16 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'sportsman','person@sports.com','hellothere'),(2,'politician','person@politics.com','hellothere'),(3,'actor','person@actor.com','hellothere'),(4,'bureaucrat','person@goi.com','hellothere'),(5,'gov_employee','person@goi.com','hellothere'),(6,'pri_employee','person@private.com','hellothere'),(7,'common_man','person@common.com','hellothere');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `user_following`
@@ -130,6 +177,16 @@ CREATE TABLE `user_following` (
   CONSTRAINT `user_following_ibfk_2` FOREIGN KEY (`following_user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_following`
+--
+
+LOCK TABLES `user_following` WRITE;
+/*!40000 ALTER TABLE `user_following` DISABLE KEYS */;
+INSERT INTO `user_following` VALUES (2,1),(3,1),(4,1),(5,1),(6,1),(7,1),(1,2),(3,2),(4,2),(5,2),(7,2),(1,3),(2,3),(4,3),(5,3),(6,3),(7,3),(3,4),(5,4),(7,4),(4,5),(7,5),(5,6),(7,6);
+/*!40000 ALTER TABLE `user_following` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -140,4 +197,4 @@ CREATE TABLE `user_following` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-04-23  2:07:10
+-- Dump completed on 2023-04-25  6:34:36
--- src/main/java/project/news_feed_cli/NewsFeedCliApplication.java
+++ src/main/java/project/news_feed_cli/NewsFeedCliApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NewsFeedCliApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(NewsFeedCliApplication.class, args);
     }
